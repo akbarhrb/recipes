5f2f09cc31001bf7f78188481138ef63f41a8ef7
ali akbar harb <aliakbar.harb@gmail.com>
2024-08-22 12:11:36 +0300

    test uploading

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Noor Food</title>
 </head>
 <body>
-    <h1>here we go again</h1>
+    
 </body>
 </html>
